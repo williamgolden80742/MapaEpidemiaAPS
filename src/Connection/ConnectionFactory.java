f91f674c6cef3ba7fb0fd56b72ebfa239cf62cb0
williamgolden80742 <52113843+williamgolden80742@users.noreply.github.com>
2020-04-21 17:17:13 -0300

    comentando linda de criação de casos

--- src/Connection/ConnectionFactory.java
+++ src/Connection/ConnectionFactory.java
@@ -23,7 +23,7 @@ public class ConnectionFactory {
     private static final String URL = "jdbc:mysql://localhost:3306/aps";
     private static final String USER = "root";
     private static final String PASS = ""; 
-//    123789190116tha
+    
     public static Connection getConnection() {
         
         try {
